0fff529c46f6823f25668d2da079ddfc8f97061d
ckdydgus123 <ckdydgus12345@gmail.com>
2020-11-03 17:57:32 +0900

    titlize

--- src/App.js
+++ src/App.js
@@ -56,8 +56,12 @@ class App extends Component {
               if(loading) return 'Loading...'
               if(error) return `Error...!! ${error.message}`
 
-              console.log({data})
-              return <div></div>
+              const search = data.search
+              const repositoryCount = search.repositoryCount
+              const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
+              const title = `GitHub Repositorys Search Reulst - ${repositoryCount} ${repositoryUnit}`
+
+              return <h2>{title}</h2>
             }
           }
         </Query>
