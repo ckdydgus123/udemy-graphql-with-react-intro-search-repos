51173e68babde57726f85808de2b300356896cf0
ckdydgus123 <ckdydgus12345@gmail.com>
2020-11-04 10:27:41 +0900

    previous button

--- src/App.js
+++ src/App.js
@@ -42,6 +42,15 @@ class App extends Component {
     })
   }
 
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfostartCursor
+    })
+  }
+
   render() {
     const { query, first, last, before, after } = this.state
     console.log({query})
@@ -93,7 +102,15 @@ class App extends Component {
                       })
                     }
                   </ul>
-                  
+                                    
+                  {
+                    search.pageInfo.hasPreviousPage == true ?
+                      <button onClick={this.goPrevious.bind(this, search)}>
+                        Previous
+                      </button>
+                      :
+                      null
+                  }
                   {
                     search.pageInfo.hasNextPage == true ?
                       <button onClick={this.goNext.bind(this, search)}>
